02730e755f1234a0985153f28cc92630207714f1
Michelle Davis <67066019+MHargita@users.noreply.github.com>
2020-09-25 21:00:31 -0400

    define total profit/losses

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,14 +3,28 @@ import os
 import csv
 
 # Set path for the file
-csv_file = os.path.join("..", "resources", "budget_data.csv")
+csvpath = os.path.join('Resources', 'budget_data.csv')
+
+# Define the function and have it accept the 'profit_losses' as its sole parameter
+def print_budget_stats(bank_data):
+    date = str(bank_data[0])
+    profit_losses = int(bank_data[1])
+    
+def total_profitlosses(bank_data):
+    profit_losses = sum(int(bank_data[1]))
+
+
+    # Total profit_losses can be found by adding the sum together
+       
 
 # Open CSV
-with open(csvpath) as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=",")
+with open(csvpath) as budget_csv_file:
+    csvreader = csv.reader(budget_csv_file, delimiter=",")
 
+
+print(print_budget_stats(bank_data))
 # Find the total number of months included in the dataset
-    for row in csvreader:
+ 
         
 
 
