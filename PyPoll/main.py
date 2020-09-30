ed4101c7087cfe538a5be2089d5688b583f876fd
Michelle Davis <67066019+MHargita@users.noreply.github.com>
2020-09-29 22:58:38 -0400

    pypoll election results

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -8,5 +8,39 @@ csvpath = os.path.join('.', 'Resources', 'election_data.csv')
 with open(csvpath, 'r') as election_data_file:
     csv_reader = csv.reader(election_data_file, delimiter= ",")
 
+#Skip header row
+    csv_header = next(csv_reader)
 
+#Set variables, candidate_votes as empty dictionary
+    Total_votes = 0
+    result = ""
+    Candidate_votes = {}
 
+    print("Election Results")
+
+#For loop to determine total amount of votes
+    for row in csv_reader:
+  
+        Total_votes += 1
+        if row[2] in Candidate_votes:
+            Candidate_votes[row[2]] += 1
+
+        else:
+            Candidate_votes[row[2]] = 1
+
+    print("-------------------------")
+    print("Total Votes: " + str(Total_votes))
+    print("-------------------------")
+
+# For loop with dictionary
+    for candidate, vote_count in Candidate_votes.items():
+        vote_percentage = vote_count * 100 / Total_votes
+        result = result + (candidate + ": " + str(round(vote_percentage, 3)) + "% (" + str(vote_count) + ")\n")
+
+# To determine winner get max votes    
+    winner = max(Candidate_votes, key = Candidate_votes.get)
+
+    print(result.rstrip("\n"))
+    print("-------------------------")
+    print("Winner: " + winner)
+    print("-------------------------")
